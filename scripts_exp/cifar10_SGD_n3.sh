40c2532470e89dafd50da84d0ef5c783f94d9467
Kan HUANG <kan.huang@connect.ust.hk>
2022-12-01 15:08:33 +0800

    sort out scripts

--- scripts_exp/cifar10_SGD_n3.sh
+++ scripts_exp/cifar10_SGD_n3.sh
@@ -12,5 +12,5 @@ for ((i=1;i<=5;i++)); do
 # --lr_schedule=cifar10_scheduler
 # discuss on data_augmentation
 # --data_augmentation=pad_crop, random_translation
-python ./scripts/train_resnet_cifar10_tf.py --n=3 --version=1 --dataset=cifar10 --data_preprocessing=std_norm --data_augmentation=pad_crop --validation_split=0.1 --batch_size=128 --epochs=200 --learning_rate=0.1 --optimizer_name=SGD --momentum=0.9 --lr_schedule=cifar10_scheduler --date_time=${date_time} --run=${i} --gpu=0;
+python ./scripts/train_resnet_cifar10_tf.py --n=3 --version=1 --dataset=cifar10 --data_preprocessing=std_norm --data_augmentation=random_translation --validation_split=0.1 --batch_size=128 --epochs=200 --learning_rate=0.1 --optimizer_name=SGD --momentum=0.9 --lr_schedule=cifar10_scheduler --date_time=${date_time} --run=${i} --gpu=0;
 done
